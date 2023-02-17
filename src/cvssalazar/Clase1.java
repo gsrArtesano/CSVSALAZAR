9bfbecae5bc161b3522402747d023fe4ef0a2be3
datilmast3r <48599451+datilmast3r@users.noreply.github.com>
2023-02-17 09:42:15 +0100

    primercomit

---
+++ src/cvssalazar/Clase1.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package cvssalazar;
+
+/**
+ *
+ * @author lobol
+ */
+public class Clase1 {
+    public void hola(){
+        System.out.println("GONZALO SALAZAR ROCHA");
+    }
+}
