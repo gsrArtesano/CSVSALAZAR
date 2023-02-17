d72756e86b28b725c669771d1d2d2a6fbee6a5d3
datilmast3r <48599451+datilmast3r@users.noreply.github.com>
2023-02-17 09:51:18 +0100

    clase2

---
+++ src/cvssalazar/Clase2.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package cvssalazar;
+
+/**
+ *
+ * @author lobol
+ */
+public class Clase2 {
+    public void holaProduccion(){
+        System.out.println("rama produccion");
+    }
+}
